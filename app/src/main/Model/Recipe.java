853b17aec150d34162a65ab95aed5fc1d634400c
RylRizqi16 <18223084@std.stei.itb.ac.id>
2025-06-03 19:12:03 +0700

    Add Admin and Resep

---
+++ app/src/main/Model/Recipe.java
@@ -0,0 +1,29 @@
+package main.Model;
+
+public class Recipe {
+    private int IDResep;
+    private String IsiResepl;
+    private Date Tanggal;
+    private Pelanggan Pelanggan;
+
+    public Recipe(int IDResep, String IsiResep, Date Tanggal, Pelanggan Pelanggan) {
+        this.IDResep = IDResep;
+        this.IsiResep = IsiResep;
+        this.Tanggal = Tanggal;
+        this.Pelanggan = Pelanggan;
+    }
+
+    // Getters dan Setters
+    public int getIDResep() {return IDResep;}
+    public String getIsiResep() {return IsiResep;}
+    public Date getTanggal() {return Tanggal;}
+    public Pelanggan getPelanggan() {return Pelanggan;}
+    public void setIDResep(int IDResep) {this.IDResep = IDResep;}
+    public void setIsiResep(String IsiResep) {this.IsiResep = IsiResep;}
+    public void setTanggal(Date Tanggal) {this.Tanggal = Tanggal;}
+    public void setPelanggan(Pelanggan Pelanggan) {this.Pelanggan = Pelanggan;}
+
+    public String ambilRiwayatResep(){
+        return "ID Resep: " + IDResep + ", Isi Resep: " + IsiResep + ", Tanggal: " + Tanggal.toString() + ", Pelanggan: " + Pelanggan.getNama();
+    }
+}
