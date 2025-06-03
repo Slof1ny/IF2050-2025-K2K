92f2e8f6306292eda0e6b9c6ddf132659263620f
furhuruhun <18223004@std.stei.itb.ac.id>
2025-06-03 19:08:44 +0700

    add Class Dokter

---
+++ app/src/main/Model/Dokter.java
@@ -0,0 +1,47 @@
+package Main.Model;
+
+public class Dokter {
+    private String nama;
+    private String spesialisasi;
+    private int id;
+
+    public Dokter(String nama, String spesialisasi, int id) {
+        this.nama = nama;
+        this.spesialisasi = spesialisasi;
+        this.id = id;
+        // Constructor for Dokter class
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getSpesialisasi() {
+        return spesialisasi;
+    }
+
+    public void setSpesialisasi(String spesialisasi) {
+        this.spesialisasi = spesialisasi;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Dokter{" +
+                "nama='" + nama + '\'' +
+                ", spesialisasi='" + spesialisasi + '\'' +
+                ", id=" + id +
+                '}';
+    }
+}
